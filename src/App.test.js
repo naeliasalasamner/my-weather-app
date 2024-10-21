86f5c5a9afb4cd30b32c91f6928fd6acd774b1d8
Naelia Salas <naelia.salas@enspirit.dev>
2024-10-21 15:57:09 +0200

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className='App'>
+<h1>HEJ</h1>
+    </div>
+  );
+}
+
+export default App;
