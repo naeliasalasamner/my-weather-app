bce72e2acc688e49d88e900625e7be511951b06d
Naelia Salas <naelia.salas@enspirit.dev>
2024-10-21 16:16:10 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
-  const apiKey = process.env.REACT_WEATHER_AP;
+  const apiKey = process.env.REACT_WEATHER_APP;
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`
 
